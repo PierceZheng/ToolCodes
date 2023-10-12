7ca60033e1f2e14ddc3ea1496ae38badaaf7e32e
pierce <pierce@fiture.com>
2023-10-12 10:55:21 +0800

    init:SwiftExample

---
+++ SwiftExample/Pods/Target Support Files/Pods-SwiftExample-SwiftExampleUITests/Pods-SwiftExample-SwiftExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftExample_SwiftExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftExample_SwiftExampleUITests
+@end
---
+++ SwiftExample/SwiftExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftExample
+//
+//  Created by 郑印平 on 2023/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
